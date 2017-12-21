1d6fc34e32d7f09fc6618d89df9f2b1d93ee355e
naveendk <naveenkolambage@gmail.com>
2017-12-21 15:04:56 +1300

    adding post message function to send from react native to webview

--- testWebBridge.js
+++ testWebBridge.js
@@ -4,22 +4,43 @@ import {
   StyleSheet,
   Text,
   View,
-  WebView
+  WebView,
+  TouchableHighlight
 } from 'react-native';
 
 //const htmlURL =require('./assets/index.html').uri;
 export default class testWebBridge extends React.Component {
+  constructor( props ) {
+    super( props );
+
+    this.webView = null;
+}
+
+onMessage( event ) {
+    console.log( "On Message", event.nativeEvent.data );
+}
+
+sendPostMessage() {
+    console.log( "Sending post message" );
+    this.webView.postMessage( "Post message from react native" );
+}
 
   render() {
   
     return (
-     
-       <WebView
-      
-        source={{uri:'https://blooming-tor-31315.herokuapp.com/'}}
-         style={{ marginTop: 20  }} 
-        />
-   
+     <View style= {{flex:1}}>
+          <TouchableHighlight style={{padding: 10, backgroundColor: 'blue', marginTop: 20}} onPress={() => this.sendPostMessage()}>
+              <Text style={{color: 'white'}}>Send post message from react native</Text>
+          </TouchableHighlight>
+
+          <WebView
+          
+            source={{uri:'https://blooming-tor-31315.herokuapp.com/'}}
+            style={{ marginTop: 20  }} 
+            ref={( webView ) => this.webView = webView}
+            onMessage={this.onMessage}
+            />
+      </View>
    )
   }
 }
